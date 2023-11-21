593f9a4cda4c56dd5a1ed1db01ff5f344d3cb760
Gene Hua <gene.y.hua@gmail.com>
2023-11-21 17:31:32 -0500

    defaults

--- site.js
+++ site.js
@@ -1,3 +1,100 @@
+const defaults = {
+  header_font_family: "'Replica LL'",
+  header_font_weight: "400",
+  paragraph_font_family: "'Times New Roman'",
+  paragraph_font_weight: "400",
+  paragraph_max_width: "",
+  text_align: "left",
+  content_max_width: "1600px",
+  breakpoint_lg: "1024px",
+  breakpoint_md: "768px",
+  breakpoint_sm: "600px",
+  page_margin_xl: "10vw / 3",
+  page_margin_lg: "10vw / 3",
+  page_margin_md: "10vw / 3",
+  page_margin_sm: "16px",
+  column_gap_xl: "10vw / 4",
+  column_gap_lg: "10vw / 4",
+  column_gap_md: "10vw / 4",
+  column_gap_sm: "16px",
+  content_tile_margin_xl: "10vw / 5",
+  content_tile_margin_lg: "10vw / 5",
+  content_tile_margin_md: "10vw / 5",
+  content_tile_margin_sm: "10px",
+  h1_fontSize_xl: "6vw",
+  h1_fontSize_lg: "3vw + 10px",
+  h1_fontSize_md: "40px",
+  h1_fontSize_sm: "28px",
+  h1_lineHeight_xl: "1",
+  h1_lineHeight_lg: "1",
+  h1_lineHeight_md: "1",
+  h1_lineHeight_sm: "1.1",
+  h1_letterSpacing_xl: "-0.01em",
+  h1_letterSpacing_lg: "-0.01em",
+  h1_letterSpacing_md: "-0.01em",
+  h1_letterSpacing_sm: "-0.01em",
+  h2_fontSize_xl: "61px",
+  h2_fontSize_lg: "2.5vw",
+  h2_fontSize_md: "28px",
+  h2_fontSize_sm: "22px",
+  h2_lineHeight_xl: "1",
+  h2_lineHeight_lg: "1.1",
+  h2_lineHeight_md: "1.3",
+  h2_lineHeight_sm: "1.4",
+  h2_letterSpacing_xl: "-0.02em",
+  h2_letterSpacing_lg: "-0.02em",
+  h2_letterSpacing_md: "-0.01em",
+  h2_letterSpacing_sm: "-0.01em",
+  h3_fontSize_xl: "40px",
+  h3_fontSize_lg: "40px",
+  h3_fontSize_md: "20px",
+  h3_fontSize_sm: "18px",
+  h3_lineHeight_xl: "1",
+  h3_lineHeight_lg: "1.1",
+  h3_lineHeight_md: "1.3",
+  h3_lineHeight_sm: "1.4",
+  h3_letterSpacing_xl: "-0.01em",
+  h3_letterSpacing_lg: "-0.01em",
+  h3_letterSpacing_md: "-0.01em",
+  h3_letterSpacing_sm: "-0.01em",
+  h4_fontSize_xl: "28px",
+  h4_fontSize_lg: "24px",
+  h4_fontSize_md: "18px",
+  h4_fontSize_sm: "16px",
+  h4_lineHeight_xl: "1",
+  h4_lineHeight_lg: "1.1",
+  h4_lineHeight_md: "1.3",
+  h4_lineHeight_sm: "1.4",
+  h4_letterSpacing_xl: "-0.01em",
+  h4_letterSpacing_lg: "-0.01em",
+  h4_letterSpacing_md: "-0.01em",
+  h4_letterSpacing_sm: "-0.01em",
+  pLg_fontSize_xl: "20px",
+  pLg_fontSize_lg: "20px",
+  pLg_fontSize_md: "16px",
+  pLg_fontSize_sm: "16px",
+  pLg_lineHeight_xl: "1.3",
+  pLg_lineHeight_lg: "1.3",
+  pLg_lineHeight_md: "1.4",
+  pLg_lineHeight_sm: "1.4",
+  pLg_letterSpacing_xl: "0",
+  pLg_letterSpacing_lg: "0",
+  pLg_letterSpacing_md: "0",
+  pLg_letterSpacing_sm: "0",
+  pSm_fontSize_xl: "16px",
+  pSm_fontSize_lg: "16px",
+  pSm_fontSize_md: "14px",
+  pSm_fontSize_sm: "14px",
+  pSm_lineHeight_xl: "1.4",
+  pSm_lineHeight_lg: "1.4",
+  pSm_lineHeight_md: "1.5",
+  pSm_lineHeight_sm: "1.5",
+  pSm_letterSpacing_xl: "0",
+  pSm_letterSpacing_lg: "0",
+  pSm_letterSpacing_md: "0",
+  pSm_letterSpacing_sm: "0",
+};
+
 const vars = document.getElementById("vars");
 const breakpoints = document.getElementById("breakpoints");
 var localStyles = JSON.parse(localStorage.getItem("styleInputs"));
@@ -80,16 +177,24 @@ function updateStyles() {
   // update CSS variables and breakpoints
   updateCSS(localStyles);
 
-  // console.log(localStyles);
+  console.log(localStyles);
+}
+
+function resetStyles() {
+  updateStyleDefinitions(JSON.stringify(defaults));
+
+  updateExportTextarea(localStyles);
+  updateStyleInputs(localStyles);
+  updateCSS(localStyles);
 }
 
 function updateStylesInitial() {
   // on load, update inputs and variables from local storage
-  updateStyleInputs(localStyles);
-  updateCSS(localStyles);
+  let styles = localStyles ? localStyles : defaults;
+  updateStyleInputs(styles);
+  updateCSS(styles);
 
   // populate export field
-  let styles = readStylesFromInputs();
   updateExportTextarea(styles);
 }
 
